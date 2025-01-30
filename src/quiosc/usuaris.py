b4be54f70d41d286bd987f0073958123fc0f0987
Aina1812 <aina.aguilera@estudiantat.upc.edu>
2025-01-30 15:39:40 +0100

    usuaris

---
+++ src/quiosc/usuaris.py
@@ -0,0 +1,10 @@
+#Registrar usuaris amb base de dades
+base_dades=[{"nom":"Carlota","naixement":"2005","telefon":"652900676"}]
+def registrar_usuari(base_dades):
+  usuari = input("Escrigui el seu nom d'usuari: ")
+  any_neixament = input('Escrigui el seu any de neixament: ')
+  telèfon = input('Escrigui el seu número de telèfon: ')
+  print('Usuari registrat amb éxit')
+  nou_usuari={"nom":usuari,"naixement":any_neixament,"telefon":telèfon}
+  base_dades=base_dades.append(nou_usuari)
+  return base_dades

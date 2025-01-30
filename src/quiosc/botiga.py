2630203ad703918b64a2c4210df9660a205d4d0d
Xavizipwav <xavier.moreno.rubio@estudiantat.upc.edu>
2025-01-30 22:27:02 +0100

    nou quiosc sense inputs

---
+++ src/quiosc/botiga.py
@@ -0,0 +1,45 @@
+# Variables
+productes= [
+                { "quantitat": "12",
+                  "nom":"coca-cola zero",
+                  "preu": 2,
+                  "categoria": "beguda"
+                },
+                { "quantitat": "3",
+                  "nom":"coca-cola",
+                  "preu": 2.1,
+                  "categoria": "beguda"
+                },
+                { "quantitat": "6",
+                  "nom":"cervesa A",
+                  "preu": 3,
+                  "categoria": "beguda"
+                },
+                { "quantitat": "0",
+                  "nom":"cervesa B",
+                  "preu": 3,
+                  "categoria": "beguda"
+                },
+                { "quantitat": "32",
+                  "nom":"oli verge",
+                  "preu": 20,
+                  "categoria": "olis"
+                },
+                { "quantitat": "16",
+                  "nom":"cervesa C",
+                  "preu": 3,
+                  "categoria": "beguda"
+                },
+                { "quantitat": "18",
+                  "nom":"oli verge extra",
+                  "preu": 22,
+                  "categoria": "olis"
+                },
+                { "quantitat": "0",
+                  "nom":"fanta",
+                  "preu": 1.5,
+                  "categoria": "beguda"
+                }
+                ]
+
+#Ordenador de preus

8b2c5eb7b7ded48d5f30d2f352498518a4869e0c
Aina1812 <aina.aguilera@estudiantat.upc.edu>
2025-01-30 18:18:53 +0100

    registrar

--- src/quiosc/usuaris.py
+++ src/quiosc/usuaris.py
@@ -1,19 +1,13 @@
 #Registrar usuaris amb base de dades
-base_dades=[{"nom":"Carlota","naixement":"2005","telefon":"652900676"}]
-def registrar_usuari(base_dades):
-  usuari = input("Escrigui el seu nom d'usuari: ")
-  any_neixament = input('Escrigui el seu any de neixament: ')
-  telèfon = input('Escrigui el seu número de telèfon: ')
-  print('Usuari registrat amb éxit')
-  nou_usuari={"nom":usuari,"naixement":any_neixament,"telefon":telèfon}
-  base_dades=base_dades.append(nou_usuari)
-  return base_dades
+base_dades=[{"nom":"Carlota","naixement":"2005","telefon":"652900676"},{"nom":"Aina","naixement":"2005","telefon":"644718544"},{"nom":"Xavi","naixement":"2005","telefon":"677865454"}]
+def registrar_usuari():
+    usuari = input("Escrigui el seu nom d'usuari: ")
+    any_neixament = input('Escrigui el seu any de neixament: ')
+    telèfon = input('Escrigui el seu número de telèfon: ')
+    print('Usuari registrat amb éxit')
+    nou_usuari={"nom":usuari,"naixement":any_neixament,"telefon":telèfon}
+    base_dades.append(nou_usuari)
+    return
 
-  #Registrar usuari
-  def registrar_usuari():
-  usuari = input("Escrigui el seu nom d'usuari: ")
-  any_neixament = input('Escrigui el seu any de neixament: ')
-  telèfon = input('Escrigui el seu número de telèfon: ')
-  print('Usuari registrat amb éxit')
-  perfil={"Nom usuari":{usuari}, "Any neixament": {any_neixament}, "Contacte": {telèfon} }
-  return perfil
+registrar_usuari()
+print(base_dades)

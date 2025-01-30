15b8a9826ed240850c26bbb1ed4164ec7cada8b2
Xavizipwav <xavier.moreno.rubio@estudiantat.upc.edu>
2025-01-30 22:53:39 +0100

    botiga v1.0

--- src/quiosc/botiga.py
+++ src/quiosc/botiga.py
@@ -43,3 +43,64 @@ productes= [
                 ]
 
 #Ordenador de preus
+def ordena_productes(productes):
+    productes = sorted(productes, key = lambda d:d["preu"])
+    return productes
+
+#Catàleg
+def cataleg(productes):
+    print("~CATÀLEG~")
+    for element in productes:
+      print(element["nom"], "-", element["preu"], "$", "-", element["quantitat"], "unitats disponibles")
+
+#Categories existents
+def categories_disponibles(productes):
+    llista_categories=[]
+    for element in productes:
+        if element["categoria"] not in llista_categories:
+           llista_categories.append(element["categoria"])
+    return llista_categories
+
+#Categoria d'un producte
+def categoria_un_producte(productes, elProducte):
+    for element in productes:
+        if element["nom"]==elProducte:
+            categ=element["categoria"]
+            return categ
+    return "No disposem d'aquest producte"
+
+#Productes d'una categoria
+def producte_categoria(productes, category):
+    producte_trobat = False
+    for element in productes:
+      if element["categoria"]==category:
+          producte_trobat = True
+          print(element["nom"], "-", element["preu"], "$", "-", element["quantitat"], "unitats disponibles")
+      if not(producte_trobat):
+          print("Ho sentim, no disposem d'aquesta categoria")
+
+#Registre d'usuari a la base de dades
+def registrar_usuari(usuari, any, telefon):
+  print('Usuari registrat amb éxit')
+  perfil={"Nom usuari":{usuari}, "Any neixament": {any}, "Contacte": {telefon} }
+  return perfil
+
+#Proves
+
+#productes=ordena_productes(productes)
+#cataleg(productes)
+
+
+#llista_categories=categories_disponibles(productes)
+#print(llista_categories)
+
+
+#laCategoria = categoria_un_producte(productes,"xocolata")
+#print(laCategoria)
+
+
+#producte_categoria(productes,"beguda")
+
+
+#perfil=registrar_usuari("Xavi", 2005, 123456789)
+#print(perfil)

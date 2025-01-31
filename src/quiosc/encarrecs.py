8f81186055112a62891e2adbdb4fe991e5ed07dd
carlottagv <carlota.grino@estudiantat.upc.edu>
2025-01-31 18:00:38 +0100

    comentari encarrecs

--- src/quiosc/encarrecs.py
+++ src/quiosc/encarrecs.py
@@ -12,14 +12,21 @@ usuaris = [
             "quantitat": 2}]
     }]
 
+#entrem ja uns usuaris manualment, amb unes comandes inicials
+
 def encarrec(usuaris, nomUsuari, laComanda):
   usuari = "carlota"
   usuari.append(laComanda)
 
+# definim una funció encarrec amb les variables del vector usaris per anar afegint els nous encàrrecs, 
+#l'usuari que fa l'encarrec i la comanda amb les quantitas que vol
 
 comanda = [{"comanda" : "coca-cola",
             "quantitat" : 2},
            {"comanda": "oli",
             "quantitat": 1}]
 
-encarrec(usuaris,"carlota", comanda) 
+
+#definim una comanda del nou usuari 
+
+encarrec(usuaris,"carlota", comanda) #fem un nou encarrec al perfil carlota
